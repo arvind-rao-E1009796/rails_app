d9c9fc5cd811527f2a48187b8e8da4146655c86f
arvind-rao-E1009796 <arvindkumar.rao@freshworks.com>
2023-12-06 11:26:44 +0530

    resource controller

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ RailsApp::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :students
 
   # Sample resource route with options:
   #   resources :products do
