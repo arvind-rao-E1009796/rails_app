8180838f7eda91cdeacfd1d7aa68125d7ee09614
arvind-rao-E1009796 <arvindkumar.rao@freshworks.com>
2023-12-06 11:42:42 +0530

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+student = Student.create(first_name: 'Arvind', last_name: 'rao', email:'Arvind@example.com')
+student = Student.new(first_name: 'Arvind', last_name: 'rao', email:'Arvind@example.com')
+student.save
+
+20.times do |idx| 
+    puts "createing student #{idx+1}"
+    Student.create(first_name: "Student #{idx+1}", last_name: 'Kumar', email:"Student #{idx+1}@example.com")
+end
