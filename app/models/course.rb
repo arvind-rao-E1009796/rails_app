3f53107d14588517e659be34369b71367787d5bc
arvind-rao-E1009796 <arvindkumar.rao@freshworks.com>
2023-12-07 00:00:22 +0530

    callbacks

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,15 @@
 class Course < ActiveRecord::Base
+    
+    before_create :greetings_message_start
+    after_save :greetings_message_end
+
+    private
+    def greetings_message_start
+        puts "Creating courses..."
+    end
+    def greetings_message_end
+        puts "Created Successfully!"
+    end
 end
+
+
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student < ActiveRecord::Base
+    after_create :display_message
     validates :first_name, :last_name, :email, :presence => true
     has_many :blogs
+    private
+    def display_message
+        puts "Hello #{self.first_name} #{self.last_name}!!"
+    end
 end
