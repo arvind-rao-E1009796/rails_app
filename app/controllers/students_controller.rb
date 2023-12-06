514cebe63a5ebbddd4f0ca0c53ebd6977c3dd7f7
arvind-rao-E1009796 <arvindkumar.rao@freshworks.com>
2023-12-06 13:09:56 +0530

    form_for_model

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,24 @@ class StudentsController < ApplicationController
     def index
         @students = Student.all;
     end
+
+
+    def new
+        @student = Student.new;
+    end
+
+    def create
+        @student = Student.new(
+            first_name: params[:student][:first_name],
+            last_name: params[:student][:last_name],
+            email: params[:student][:email]
+        );
+        if @student.save
+            redirect_to students_path
+          else
+            # This line overrides the default rendering behavior, which
+            # would have been to render the "create" view.
+            render :action => "new"
+          end
+    end
 end
