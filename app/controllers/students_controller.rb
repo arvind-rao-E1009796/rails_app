68c240480e9e06c61e7b1ac99ca79bb401a4ea26
arvind-rao-E1009796 <arvindkumar.rao@freshworks.com>
2023-12-06 14:40:14 +0530

    before_action_callback

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,7 @@
 class StudentsController < ApplicationController
+
+    before_action :set_student, only: [:show, :edit, :update, :destroy]
+
     def index
         @students = Student.all;
     end
@@ -17,23 +20,17 @@ class StudentsController < ApplicationController
         if @student.save
             redirect_to students_path
           else
-            # This line overrides the default rendering behavior, which
-            # would have been to render the "create" view.
             render :action => "new"
           end
     end
 
     def show
-        @student = Student.find(params[:id]);
-
     end
 
     def edit 
-        @student = Student.find(params[:id]);
     end
 
     def update
-        @student = Student.find(params[:id]);
         if @student.update_attributes(            
                 first_name: params[:student][:first_name],
                 last_name: params[:student][:last_name],
@@ -46,9 +43,13 @@ class StudentsController < ApplicationController
     end
 
     def destroy
-        @student = Student.find(params[:id]);
         @student.destroy
         redirect_to students_path 
     end
+
+    private
+
+    def set_student
+        @student = Student.find(params[:id]);
 
 end
