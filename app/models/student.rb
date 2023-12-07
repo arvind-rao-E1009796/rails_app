1df0f7acf3de1689c47b5d918ed40cf06e0b7821
arvind-rao-E1009796 <arvindkumar.rao@freshworks.com>
2023-12-07 10:51:26 +0530

    instance_method

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,6 +5,11 @@ class Student < ActiveRecord::Base
     validates :first_name, :last_name, length: {minimum: 1, maximum:10}
     validate :custom_validations_email
     has_many :blogs
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
+
     private
     def display_message
         puts "Hello #{self.first_name} #{self.last_name}!!"
