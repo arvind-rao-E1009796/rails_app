7744ec2cd4a3bbf1af468562eaf40ddafcb87961
arvind-rao-E1009796 <arvindkumar.rao@freshworks.com>
2023-12-07 10:47:24 +0530

    add validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,21 @@
 class Student < ActiveRecord::Base
     after_create :display_message
     validates :first_name, :last_name, :email, :presence => true
+    validates :email, :uniqueness => true
+    validates :first_name, :last_name, length: {minimum: 1, maximum:10}
+    validate :custom_validations_email
     has_many :blogs
     private
     def display_message
         puts "Hello #{self.first_name} #{self.last_name}!!"
     end
+
+    def custom_validations_email
+        if self.email.include?('@')
+            puts "valid email"
+        else
+            errors.add(:email, 'Invalid email ! Must include @')
+        end
+    end
+
 end
